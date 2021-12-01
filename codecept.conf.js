d988b4d8e74a0cde99166cc48946cd08044e23af
Poliana Rufatto <poliana.rufatto@softplan.com.br>
2021-12-01 08:42:24 +0100

    first commit

---
+++ codecept.conf.js
@@ -0,0 +1,36 @@
+exports.config = {
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js',
+    loginPagePage: './pages/loginPage.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.js']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    }
+  },
+  tests: './*_test.js',
+  name: 'demo-codecept-cucumber'
+}
